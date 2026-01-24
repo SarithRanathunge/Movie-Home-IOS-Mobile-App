65ff40af98074edb7ec25073eb2bb1a835edeb22
Sarith Ranathunge <sarithperera14@gmail.com>
2026-01-24 15:48:46 +0530

    Initial Commit

---
+++ MovieHome/MovieHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieHomeApp.swift
+//  MovieHome
+//
+//  Created by Sarith Ranathunge on 2026-01-24.
+//
+
+import SwiftUI
+
+@main
+struct MovieHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
